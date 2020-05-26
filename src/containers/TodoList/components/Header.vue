b9208d655c5352483ab712ad379a2f0b9f7fb049
zr <247076126@qq.com>
2020-05-26 23:36:02 +0800

    udpoate

--- src/containers/TodoList/components/Header.vue
+++ src/containers/TodoList/components/Header.vue
@@ -1,15 +1,26 @@
+
+<style lang="less" scoped>
+</style>
 <template>
   <div>
-
+<input type="text" data-test="input" v-model="inputValue" @keyup.enter="addTodoItem" >
   </div>
 </template>
 
 <script>
   export default {
-    
+      data() {
+        return {
+          inputValue:''
+        }
+      },
+      methods: {
+        addTodoItem(){
+          if(this.inputValue){
+            this.$emit('add',this.inputValue);
+            this.inputValue = '';
+          }
+        }
+      },
   }
 </script>
-
-<style lang="less" scoped>
-
-</style>
